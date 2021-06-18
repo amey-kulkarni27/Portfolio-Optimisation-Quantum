55d1a380bc9ef34bfb8123b422740e2fabe6073d
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-06-19 01:37:17 +0530

    fixed stocks

--- fixed_stocks.py
+++ fixed_stocks.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from collections import defaultdict
 import pandas as pd
-# from dwave.system import DWaveSampler, EmbeddingComposite
+from dwave.system import DWaveSampler, EmbeddingComposite
 
 N = 3 # Number of stocks
 f = 2 # Fixed number of stocks that can be chosen
@@ -27,4 +27,9 @@ for i in range(N):
     Q[(i, i)] += cov.iloc[i, i]
 
 for i, j in G.edges:
-    Q[(i, j)] += cov.iloc([i, j])
+    Q[(i, j)] += cov.iloc[i, j]
+
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample_qubo(Q, num_reads=10, chain_strength=1)
+
+print(sampleset)
