327e6c4f68efab536ddf26040aa076613738a9be
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-06-22 11:49:19 +0000

    Decimal optimisation, to be tested

---
+++ decimal_precision.py
@@ -0,0 +1,82 @@
+import networkx as nx
+from collections import defaultdict
+import pandas as pd
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+N = 4 # Number of stocks
+sig_p = 0.9 # Expected return from n stocks (not average currently)
+f = 3 # Fixed number of stocks that can be chosen
+precision_bits = 3 # For each stock, this is the precision of its weight
+dim = N * precision_bits # dim stands for matrix dimensions
+max_wt = 1.0 - 1.0 / pow(2, precision_bits)
+
+G = nx.Graph()
+G.add_edges_from([(i, j) for i in range(dim) for j in range(i + 1, dim)])
+
+# Create the covariance matrix and returns list
+cov = pd.read_csv("cov_matrix.csv")
+returns = pd.read_csv("mean_returns.csv")
+
+# The matrix where we add the objective and the constraint
+Q = defaultdict(int)
+
+# Constraint1 minimises the difference between expected return and actual return
+lagrange1 = 1
+for d in range(dim):
+    i = d // precision_bits # The stock number
+    p = d % precision_bits + 1 # The p^th of the bits we are using to represent the i^th stock
+    ri = returns.iloc[i] # i^th stock returns
+    Q[(d, d)] += (-2 * sig_p * ri / pow(2, p) + ri * ri / pow(2, 2 * p)) * lagrange1
+    for d_dash in range(d + 1, dim):
+        j = d_dash // precision_bits # The stock number
+        q = d_dash % precision_bits + 1 # The q^th of the bits we are using to represent the j^th stock
+        rj = returns.iloc[j] # j^th stock returns
+        Q[(d, d_dash)] += 2 * ri * rj * lagrange1 / pow(2, p + q)
+
+
+# Constraint2 specifying only f stocks should be used
+lagrange2 = 1
+for d in range(dim):
+    i = d // precision_bits # The stock number
+    p = d % precision_bits + 1 # The p^th of the bits we are using to represent the i^th stock
+    Q[(d, d)] += (-2 * f / pow(2, p) + 1 / pow(2, 2 * p)) * lagrange2
+    for d_dash in range(d + 1, dim):
+        j = d_dash // precision_bits # The stock number
+        q = d_dash % precision_bits + 1 # The q^th of the bits we are using to represent the j^th stock
+        Q[(d, d_dash)] += 2 * lagrange2 / pow(2, p + q)
+
+# The objective function
+for d in range(dim):
+    for d_dash in range(d, dim):
+        i = d // precision_bits # The stock number
+        p = d % precision_bits + 1 # The p^th of the bits we are using to represent the i^th stock
+        j = d_dash // precision_bits # The stock number
+        q = d_dash % precision_bits + 1 # The q^th of the bits we are using to represent the j^th stock
+        covij = cov.iloc[i, j]
+        if d == d_dash:
+            # i == j and p == q
+            Q[(d, d)] += 0.5 * covij / pow(2, 2 * p)
+        elif i == j:
+            # This means we are talking about the same stock, but different bits
+            Q[(d, d_dash)] += covij / pow(2, p + q)
+        else:
+            # Different stocks
+            Q[(d, d_dash)] += covij / pow(2, p + q)
+
+
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample_qubo(Q, num_reads=10, chain_strength=1)
+
+# Print the entire sampleset, that is, the entire table
+print(sampleset)
+
+# For the lowest energy, find the actual return
+actual_return = 0.0
+distribution = sampleset.first.sample
+for s_num in distribution.keys():
+    if(distribution[s_num] == 1):
+        i = s_num // precision_bits # Stock number
+        p = s_num % precision_bits + 1 # Bit number
+        actual_return += returns.iloc[i] / pow(2, p)
+
+print(actual_return)
